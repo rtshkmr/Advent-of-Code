4e743436a937748875b4486d10d039109a525a91
Ritesh Kumar <ritesh@emerald.pink>
2021-12-02 16:15:15 +0800

    Day 2 Part 2 - Submarine!

--- 2/solution.py
+++ 2/solution.py
@@ -25,11 +25,32 @@ def extractDistances(inputs):
             assert false
     return [forward_dist, depth]
 
+def extractDistancesWithAim(inputs):
+    forward_dist = 0
+    depth = 0 
+    aim = 0
+    for cmd in inputs: 
+        axis, value = cmd.split(" ")
+        value = int(value)
+        if(axis == "forward"): 
+            forward_dist += (value)
+            depth += aim * value
+        elif (axis == "up"): 
+            aim -= value
+        elif(axis == "down"): 
+            aim += value
+        else: 
+            assert false
+    return [forward_dist, depth]
+
+
 def main():
     inputs = extractInputs()
     forward_dist, depth = extractDistances(inputs)
-    answer = forward_dist *depth 
-    print(f"The answer is {answer}")
+    answer_1 = forward_dist *depth 
+    fwd_dist, d = extractDistancesWithAim(inputs)
+    answer_2 = fwd_dist * d
+    print(f"The first answer is {answer_1} and the second answer is {answer_2}")
 
 if __name__ == "__main__":
     main()
