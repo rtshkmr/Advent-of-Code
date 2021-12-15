21fc8e3c9d97669d9c63bef7c8915c9acb96b694
Ritesh Kumar <ritesh@emerald.pink>
2021-12-15 23:19:22 +0800

    Day 4 - Bingo Board Part 1

---
+++ 4/solution.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+"""
+AOC Day 4 - Bingo Board
+"""
+
+__author__ = "Ritesh Kumar"
+
+# a bingo board is predefined to be a 5x5 grid
+class BingoBoard:
+    def __init__(self, board_lines):
+        self.board_lines = board_lines
+        self.current_unmarked_sum = sum(sum(board_lines, [])) # pythonic way of summing 2d arr
+        self.has_won = False
+        self.current_pick = -1 # starting value, assuming that bingo board will never have negative nums
+        return
+
+    # checks row-wise then column wise to see if there's a win
+    def check_for_wins(self):
+        # check rows and cols in single pass:
+        for idx in range(len(self.board_lines)):
+            row = self.board_lines[idx]
+            col = [r[idx] for r in self.board_lines] # hooray for list comprehension in python
+            is_winning_row = len(set(row)) == 1 and self.board_lines[idx][0] == -1
+            is_winning_col = len(set(col)) == 1 and self.board_lines[0][idx] == -1
+            if(is_winning_col or is_winning_row):
+                print("we have ourselves a winner!")
+                self.has_won = True
+                return True
+
+    # combs thru in O(n^2)
+    def play_round(self, pick):
+        self.current_pick = pick
+        for row in range(len(self.board_lines)):
+            for col in range(len(self.board_lines[row])):
+                val = self.board_lines[row][col]
+                if (val == pick):
+                    self.current_unmarked_sum -= val
+                    self.board_lines[row][col] = -1 # marks visited
+        self.check_for_wins()
+
+    def get_score(self):
+        return self.current_unmarked_sum * self.current_pick
+
+def init_game():
+    bingo_boards = []
+    with open("sample") as f:
+        inputs = list(map(lambda x:(x.rstrip("\n")), f.readlines()))
+    picks = inputs[0].split(",")
+    board_buffer = []
+    for line in inputs[1:]:
+        if not line:
+            if board_buffer:
+                bingo_boards.append(BingoBoard(board_buffer))
+                board_buffer = [] # flush when done
+        else:
+            board_buffer.append(list(map(lambda x:int(x),line.split())))
+    if board_buffer: # last board in buffer
+        bingo_boards.append(BingoBoard(board_buffer))
+    return [picks, bingo_boards]
+
+def main():
+    picks, bingo_boards = init_game()
+    print("game has been init, let's pick and play!")
+    for pick in picks:
+        for board in bingo_boards:
+            board.play_round(int(pick))
+            if board.has_won:
+                print(f"We have ourselves a winner with a score of {board.get_score()} !")
+                return
+    return
+
+
+if __name__ == "__main__":
+    main()
